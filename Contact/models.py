e5251e1c926cfe921aef294ca705edffc1e85393
magali gosselin <magali@wertkt.com>
2020-07-18 10:19:48 +0200

    change settings

---
+++ Contact/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    fisrt_name = models.CharField(verbose_name='Nom',max_length=50)
+    last_name = models.CharField(verbose_name='Prénom',max_length=50)
+    email = models.EmailField(verbose_name='Adresse e-mail', blank=True)
+    phone_number = models.IntegerField(verbose_name='Téléphone')
+    tag = models.CharField(verbose_name='Catégorie',max_length=50)
+    created_at = models.DateTimeField(verbose_name='Date de création', auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
