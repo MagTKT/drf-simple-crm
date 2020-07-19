82ed97404e4b51b11d9c889246a27894a3d49737
magali gosselin <magali@wertkt.com>
2020-07-19 11:17:27 +0200

    change views for contact

--- Contact/tests.py
+++ Contact/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Contact
+
+
+class ContactModelTests(TestCase):
+
+    def test_verification_of_email_validate(self):
+        """
+            Verification of email validate
+        """
+        email = 'email@email.com'
+        self.assertEqual(data['email'], 'email@email.com')
--- Contact/views.py
+++ Contact/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Contact
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    contact_list = Contact.objects.order_by('-created_at')
+    context = {
+        'contact_list':contact_list
+    }
+    return render(request, 'contact/index.html', context)
+
