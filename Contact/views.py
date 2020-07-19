3ef9afb5fb0e64439cf1ee9789bd1206f74ee3cb
magali gosselin <magali@wertkt.com>
2020-07-19 13:28:50 +0200

    change views

--- Contact/views.py
+++ Contact/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Contact
+from .forms import ContactForm
 
 
-def index(request):
-    contact_list = Contact.objects.order_by('-created_at')
-    context = {
-        'contact_list':contact_list
-    }
-    return render(request, 'contact/index.html', context)
+class ContactList(ListView): 
+    model = Contact
 
+class ContactDetail(DetailView): 
+    model = Contact
+
+class ContactCreate(CreateView): 
+    model = Contact
+
+class ContactUpdate(UpdateView): 
+    model = Contact
+
+class ContactDelete(DeleteView): 
+    model = Contact
